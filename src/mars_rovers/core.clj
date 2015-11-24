979f3264e9c216152ac05a50867a028bf51df992
Joan Valduvieco <jvalduvieco@gmail.com>
2015-11-25 00:30:03 +0100

    API cleanup

--- src/mars_rovers/core.clj
+++ src/mars_rovers/core.clj
@@ -1,7 +1,7 @@
 (ns mars-rovers.core
   (:require [clojure.core.match :refer [match]]))
 
-(defn move-rover [[x y heading :as rover-state] action]
+(defn- move-rover [[x y heading :as rover-state] action]
   (match [heading action]
          [\N \M] [x (inc y) heading]
          [\E \M] [(inc x) y heading]
@@ -17,17 +17,17 @@
          [\E \L] [x y \N]
          ))
 
-(defn move-rover-with-limits [[top-x top-y :as limits] rover-state action]
+(defn- move-rover-with-limits [[top-x top-y :as limits] rover-state action]
   (let [new-rover-state (move-rover rover-state action)
         [x y _] new-rover-state]
     (if (and (>= top-x x 0) (>= top-y y 0)) new-rover-state rover-state)))
 
-(defn do-mission [map-limits [rover-state [commands] :as rover-data]]
+(defn- do-mission [map-limits [rover-state [commands] :as rover-data]]
   (reduce
     (partial move-rover-with-limits map-limits)
     rover-state commands))
 
-(defn raw-input-data->rover-state-and-commands [rover-input-data]
+(defn- raw-input-data->rover-state-and-commands [rover-input-data]
   (partition 2 rover-input-data))
 
 (defn new-mission [raw-mission-input]
