6e9d84895b3e7bee2fc71ce856d67f41e3198104
Joan Valduvieco <jvalduvieco@gmail.com>
2015-11-26 15:21:44 +0100

    Extended test cases

--- test/mars_rovers/core_test.clj
+++ test/mars_rovers/core_test.clj
@@ -4,13 +4,13 @@
 
 (deftest supplied-example
   (testing "Supplied example is met"
-  (is (= (new-mission
-          '[[5 5]
-            [1 2 \N]
-            ["LMLMLMLMM"]
-            [3 3 \E]
-            ["MMRMMRMRRM"]])
-         '([1 3 \N] [5 1 \E])))))
+    (is (= (new-mission
+             '[[5 5]
+               [1 2 \N]
+               ["LMLMLMLMM"]
+               [3 3 \E]
+               ["MMRMMRMRRM"]])
+           '([1 3 \N] [5 1 \E])))))
 
 (deftest no-rovers
   (testing "No rovers, no results"
@@ -30,12 +30,52 @@
              '[[2 2]
                [6 6 \N]
                ["LMLMLMLMM"]])
-             '([6 6 \N])))))
+           '([6 6 \N])))))
 
-(deftest bounds-are-enforced
-  (testing "a rover can not leave bounds"
+(deftest north-bound-is-enforced
+  (testing "a rover can go beyond N bound"
     (is (= (new-mission
              '[[2 2]
                [1 1 \N]
                ["MMMMMMMMMMMM"]])
            '([1 2 \N])))))
+
+(deftest east-bound-is-enforced
+  (testing "a rover can go beyond E bound"
+    (is (= (new-mission
+             '[[2 2]
+               [1 1 \E]
+               ["MMMMMMMMMMMM"]])
+           '([2 1 \E])))))
+
+(deftest west-bound-is-enforced
+  (testing "a rover can go beyond W bound"
+    (is (= (new-mission
+             '[[2 2]
+               [1 1 \W]
+               ["MMMMMMMMMMMM"]])
+           '([0 1 \W])))))
+
+(deftest south-bound-is-enforced
+  (testing "a rover can go beyond s bound"
+    (is (= (new-mission
+             '[[2 2]
+               [1 1 \S]
+               ["MMMMMMMMMMMM"]])
+           '([1 0 \S])))))
+
+(deftest rotate-left
+  (testing "a rover rotates left"
+    (is (= (new-mission
+             '[[2 2]
+               [1 1 \N]
+               ["L"]])
+           '([1 1 \W])))))
+
+(deftest rotate-right
+  (testing "a rover rotates right"
+    (is (= (new-mission
+             '[[2 2]
+               [1 1 \N]
+               ["R"]])
+           '([1 1 \E])))))
