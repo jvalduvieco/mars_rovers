a23918eb29b627454afaecf006ba234d2308d429
Joan Valduvieco <jvalduvieco@gmail.com>
2015-12-02 23:27:44 +0100

    Start using schema ( https://github.com/Prismatic/schema )

---
+++ src/mars_rovers/schemas.clj
@@ -0,0 +1,17 @@
+(ns mars-rovers.schemas
+  (:require [schema.core :as s]))
+
+(def Map
+  {:max-x s/Int :max-y s/Int})
+
+(def CardinalPoints
+  (s/enum \N \E \S \W))
+(def RoverState
+  {:x s/Int :y s/Int :heading CardinalPoints})
+
+(def RoverMission
+  {:rover-state RoverState :commands s/Str})
+
+(def RawMapDefinition
+  [(s/one s/Int "max-x") (s/one s/Int "max-y")])
+
